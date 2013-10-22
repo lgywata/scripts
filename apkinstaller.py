d4519123b55b6307d10610507097ac6aff8af1dc
ywata <luizg.ywata@intel.com>
2013-10-22 12:18:19 -0200

    adding rsync features

--- apkinstaller.py
+++ apkinstaller.py
@@ -1,12 +1,24 @@
 import os
 import sys
 
-# the path to de directory with the apks
-path = sys.argv[1] 
-if not path.endswith('/') :
-    path = path + '/'
+login = raw_input("Enter your login @master: ")
+# remote: ywata@master:/tmp
+remote = login + "@master:/tmp/apks"
 
-apks = os.listdir(path)
+# use absolute path
+localdir = raw_input("Enter the directory you want to be rsynced (empty entry will use ./): ")
+
+if not localdir:
+    localdir = "./"
+
+from subprocess import call
+call(["rsync", "-avz", remote, localdir])
+localdir = localdir + "apks/"
+
+apks = os.listdir(localdir)
+
+print ', '.join(apks)
 for apk in apks:
-    from subprocess import call
-    call(["adb", "install", path + apk])
+   from subprocess import call
+   call(["adb", "install", localdir + apk])
+
