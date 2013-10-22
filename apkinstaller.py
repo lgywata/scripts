35c5e144a24a477b76a00d5636ea80a263444d9e
ywata <luizg.ywata@intel.com>
2013-10-22 13:05:28 -0200

    handling ctr+c with try except

--- apkinstaller.py
+++ apkinstaller.py
@@ -1,24 +1,27 @@
 import os
 import sys
 
-login = raw_input("Enter your login @master: ")
-# remote: ywata@master:/tmp
-remote = login + "@master:/tmp/apks"
+try:
+    login = raw_input("Enter your login @master: ")
+    # remote: ywata@master:/tmp
+    remote = login + "@master:/tmp/apks"
 
-# use absolute path
-localdir = raw_input("Enter the directory you want to be rsynced (empty entry will use ./): ")
+    # use absolute path
+    localdir = raw_input("Enter the directory you want to be rsynced (empty entry will use ./): ")
 
-if not localdir:
-    localdir = "./"
+    if not localdir:
+        localdir = "./"
 
-from subprocess import call
-call(["rsync", "-avz", remote, localdir])
-localdir = localdir + "apks/"
+        from subprocess import call
+        call(["rsync", "-avz", remote, localdir])
+        localdir = localdir + "apks/"
+        
+        apks = os.listdir(localdir)
 
-apks = os.listdir(localdir)
-
-print ', '.join(apks)
-for apk in apks:
-   from subprocess import call
-   call(["adb", "install", localdir + apk])
+        print ', '.join(apks)
+        for apk in apks:
+            from subprocess import call
+            call(["adb", "install", localdir + apk])
+except KeyboardInterrupt:
+    sys.exit()
 
