67342f6a2d33f7c56664a6b5501b5dc00f43476d
ywata <luizg.ywata@intel.com>
2013-10-22 13:28:39 -0200

    improving syntax and checking entry

--- apkinstaller.py
+++ apkinstaller.py
@@ -5,23 +5,23 @@ try:
     login = raw_input("Enter your login @master: ")
     # remote: ywata@master:/tmp
     remote = login + "@master:/tmp/apks"
-
     # use absolute path
     localdir = raw_input("Enter the directory you want to be rsynced (empty entry will use ./): ")
 
     if not localdir:
         localdir = "./"
 
-        from subprocess import call
-        call(["rsync", "-avz", remote, localdir])
-        localdir = localdir + "apks/"
-        
-        apks = os.listdir(localdir)
+    if not localdir.endswith('/'):
+        localdir += '/'
 
-        print ', '.join(apks)
-        for apk in apks:
-            from subprocess import call
-            call(["adb", "install", localdir + apk])
+    from subprocess import call
+    call(["rsync", "-avz", remote, localdir])
+    localdir += "apks/"
+        
+    apks = os.listdir(localdir)
+    for apk in apks:
+        from subprocess import call
+        call(["adb", "install", localdir + apk])
 except KeyboardInterrupt:
     sys.exit()
 
