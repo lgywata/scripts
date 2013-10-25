15adf2b54a2cb4c2c5764e43b36d6be56b18cf92
ywata <luizg.ywata@intel.com>
2013-10-25 13:14:06 -0200

    script will now generate Android.mk and device.mk in order to compile prebuilt apks

--- apkinstaller.py
+++ apkinstaller.py
@@ -4,7 +4,7 @@ import sys
 try:
     login = raw_input("Enter your login @master: ")
     # remote: ywata@master:/tmp
-    remote = login + "@master:/tmp/apks"
+    remote = login + "@master:/tmp/apps"
     # use absolute path
     localdir = raw_input("Enter the directory you want to be rsynced (empty entry will use ./): ")
 
@@ -16,12 +16,32 @@ try:
 
     from subprocess import call
     call(["rsync", "-avz", remote, localdir])
-    localdir += "apks/"
+    localdir += "apps/"
         
+    androidmk = open(localdir + "Android.mk", "wb")
+    androidmk.write("LOCAL_PATH := $(call my-dir)\n\n")
+
+    devicemk = open("/home/ywata/workspace/build/target/product/generic_no_telephony.mk", "wb")
+    devicemk.write("\n PRODUCT_PACKAGES += \\\n");
+
     apks = os.listdir(localdir)
     for apk in apks:
-        from subprocess import call
-        call(["adb", "install", localdir + apk])
+        if not apk.endswith(".apk"):
+            continue
+        androidmk.write("include $(CLEAR_VARS)\n")
+        androidmk.write("LOCAL_MODULE := " + apk[:-4] + "\n")
+        androidmk.write("LOCAL_SRC_FILES := " + apk + "\n")
+        androidmk.write("LOCAL_MODULE_SUFFIX := .apk\n")
+        androidmk.write("LOCAL_MODULE_CLASS := APPS\n")
+        androidmk.write("LOCAL_CERTIFICATE := PRESIGNED\n")
+        androidmk.write("LOCAL_MODULE_PATH := $(TARGET_OUT)/app\n")
+        androidmk.write("include $(BUILD_PREBUILT)\n\n")
+        
+        devicemk.write("    " + apk[:-4] + " \\\n")
+
+    androidmk.close()
+    devicemk.close()
+
 except KeyboardInterrupt:
     sys.exit()
 
