4847711bd18b6e985fcb0b1e359b4a3d12432756
ywata <luizg.ywata@intel.com>
2013-11-22 13:38:32 -0200

    multithread

--- parser.py
+++ parser.py
@@ -1,26 +1,51 @@
+import threading 
 import xml.etree.ElementTree as ET
-tree = ET.parse('testResult.xml')
-root = tree.getroot()
 
-aosp = []
-for test in root.iter('Test'):
-    result = test.get('result')
-    if result == 'fail':
-        aosp.append(test.get('name'))
+class myThread (threading.Thread):
+    def __init__(self, filepath, total):
+        threading.Thread.__init__(self)
+        self.filepath = filepath
+        self.total = total
 
-tree = ET.parse('../with_patches/testResult.xml')
-root = tree.getroot()
+    def run(self):
+        parsexml(self.filepath, self.total)
 
-intel = []
-for test in root.iter('Test'):
-    result = test.get('result')
-    if result == 'fail':
-        intel.append(test.get('name'))
+total_1 = []
+total_2 = []
 
-failed = 0
-for t in intel:
-    if t not in aosp:
-        print t
-        failed += 1
+def parsexml(filepath, total):
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+    packages = []
+    tests = []
+    results = []
+    for package in root.findall('TestPackage'):
+        packages.append(package.get('name'))
+        for test in package.getiterator('Test'):
+            if test.get('result') == 'fail':
+                tests.append(test.get('name'))
+                results.append(test.get('result'))
+        packages.append(tests)
+        packages.append(results)
+        total.append(packages)
+        break
+    
 
-print "total Intel failures:", failed
+file1 = myThread('testResult.xml', total_1)
+file2 = myThread('testResult.xml', total_2)
+
+file1.start()
+file2.start()
+
+file1.join()
+file2.join()
+
+for index, val in enumerate(total_1):
+    print val[2]
+    if 'false' not in val[2]:
+        print "No failures for package: ", val[0]
+    else:
+        print "poutz..."
+    break
+
+print "Finished!\n"
